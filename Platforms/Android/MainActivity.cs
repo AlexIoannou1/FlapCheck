707156ae6263697d9cc84958f14725466f366a18
AlexIoannou1 <AlexIoannou@cosine.com.cy>
2025-07-04 09:30:31 +0300

    Initial commit after fixes

---
+++ Platforms/Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Microsoft.Maui;
+
+namespace FlapCheckApp;
+
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,                         // ⬅ tells Android to run this activity
+    ConfigurationChanges =
+        ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation |
+        ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout |
+        ConfigChanges.SmallestScreenSize |
+        ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
